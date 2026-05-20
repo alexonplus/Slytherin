b0ab3afda4579a939f7ef38ec17f6ba7d2e6a993
alexonplus <oleks9185@gmail.com>
2026-05-20 23:04:01 +0200

    all snake moving test

---
+++ SnakeGame.Tests/SnakeTest.cs
@@ -0,0 +1,49 @@
+﻿using Xunit;
+using SnakeGame;
+
+namespace SnakeGame.Tests;
+
+public class SnakeTests
+{
+    [Fact]
+    public void Move_Down_IncreasesY()
+    {
+        var snake = new Snake(10, 10);
+        snake.Direction = Direction.Down;
+
+        snake.Move();
+
+        Assert.Equal(11, snake.HeadY);
+    }
+
+    [Fact]
+    public void Move_Left_DecreasesX()
+    {
+        var snake = new Snake(10, 10);
+        snake.Direction = Direction.Left;
+        snake.Move();
+        Assert.Equal(9, snake.HeadX);
+    }
+
+    [Fact]
+    public void Move_Right_IncreasesX()
+    {
+        var snake = new Snake(10, 10);
+        snake.Direction = Direction.Right;
+        snake.Move();
+        Assert.Equal(11, snake.HeadX);
+    }
+
+    [Fact]
+    public void Move_Up_DecreasesY()
+    {
+        var snake = new Snake(10, 10);
+        snake.Direction = Direction.Up;
+        snake.Move();
+        Assert.Equal(9, snake.HeadY);
+    }
+
+
+
+
+}
